1d9d80e89f16edc8572cddafc9f06a886c885f00
Neha Singh <nehasingh250691@gmail.com>
2017-11-20 19:01:58 +0530

    Insert a document to Users collection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,16 +6,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db) =>{
     }
     console.log('Connected to MongoDB Server!');
 
-    db.collection('Todos').insertOne({
-        completed: false,
-        text: 'Something to do'
+    // db.collection('Todos').insertOne({
+    //     completed: false,
+    //     text: 'Something to do'
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to save todo', err);
+    //     }
+    //     console.log('Todo saved.')
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // })
+
+    db.collection('Users').insertOne({
+        name:'Neha',
+        age:25,
+        location:'Gurgaon'
     }, (err, result) => {
         if(err){
-            return console.log('Unable to save todo', err);
+            return console.log('Unable to save User ', err);
         }
-        console.log('Todo saved.')
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+        console.log('User saved!')
+        console.log(JSON.stringify(result.ops,undefined,2));
+    })
 
     db.close();
 });
